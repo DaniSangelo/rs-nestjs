5e1197164ff44e1526e20ffc494d459cf84d1279
Daniel Sângelo <danielsangelo@hotmail.com>
2025-01-04 14:55:56 -0300

    Fix zod validation pipe

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -7,7 +7,7 @@ export class ZodValidationPipe implements PipeTransform {
 
   transform(value: unknown) {
     try {
-      this.schema.parse(value);
+      return this.schema.parse(value);
     } catch (error) {
       if (error instanceof ZodError) {
         throw new BadRequestException(
@@ -20,7 +20,5 @@ export class ZodValidationPipe implements PipeTransform {
       }
       throw new BadRequestException('Validation failed')
     }
-
-    return value;
   }
 }
