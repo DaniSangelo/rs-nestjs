4d8329f86a0ff2948dbd671fcb68ecf2fb59cd61
Daniel Sângelo <danielsangelo@hotmail.com>
2025-01-11 14:25:55 -0300

    Remove answerid column from Question schema

---
+++ prisma/migrations/20250111171949_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `answer_id` on the `questions` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "questions" DROP COLUMN "answer_id";
