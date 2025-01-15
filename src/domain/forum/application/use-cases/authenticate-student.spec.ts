17300b76912c3eaa6dfdc41b3790e96bb4129ae4
Daniel Sângelo <danielsangelo@hotmail.com>
2025-01-15 14:13:53 -0300

    Create tests for authenticate and register student use cases

---
+++ src/domain/forum/application/use-cases/authenticate-student.spec.ts
@@ -0,0 +1,43 @@
+import { FakeEncrypter } from 'test/cryptography/fake-encrypter'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { InMemoryStudentRepository } from 'test/repositories/in-memory-student-repository'
+import { AuthenticateStudentUseCase } from './authenticate-student'
+import { makeStudent } from 'test/factories/make-student'
+
+let inMemoryStudentRepository: InMemoryStudentRepository
+let fakeHasher: FakeHasher
+let fakeEncrypter: FakeEncrypter
+let sut: AuthenticateStudentUseCase
+
+describe('Authenticate student use case', () => {
+  beforeEach(() => {
+    inMemoryStudentRepository = new InMemoryStudentRepository()
+    fakeHasher = new FakeHasher()
+    fakeEncrypter = new FakeEncrypter()
+
+    sut = new AuthenticateStudentUseCase(
+      inMemoryStudentRepository,
+      fakeHasher,
+      fakeEncrypter,
+    )
+  })
+
+  it('should be able to authenticate a student', async () => {
+    const student = makeStudent({
+      email: 'daniel@mail.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryStudentRepository.items.push(student)
+
+    const result = await sut.execute({
+      email: 'daniel@mail.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+})
